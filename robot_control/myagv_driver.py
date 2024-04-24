4d468ab2aa5ffa9b34dd4777131af8f9632a6a71
juanborbon93 <ying.zhang@elephantrobotics.com>
2024-04-24 08:43:55 +0800

    fixed velocity scaling

--- robot_control/myagv_driver.py
+++ robot_control/myagv_driver.py
@@ -5,12 +5,11 @@ from time import monotonic, sleep
 import atexit
 from pymycobot.myagv import MyAgv
 
-# Some data models representing AGV status
+
 class BatteryStatus(BaseModel):
     charging:bool = None
     voltage:float = None
     inserted:bool = None
-
 class BatteryCheck(BaseModel):
     time_checked:float = None
     battery_1:BatteryStatus = BatteryStatus()
@@ -20,13 +19,10 @@ class AGVState(BaseModel):
     x_velocity: float = 0.0
     y_velocity: float = 0.0
     rz_velocity: float = 0.0
+    # battery status is expresed as Typle[time_checked, battery_voltage]
     battery_status:BatteryCheck = BatteryCheck()
 
-
 class AGV:
-    """"
-    Driver for controlling the AGV and getting some status data from it
-    """
     def __init__(self,port:str="/dev/ttyAMA2"):
         self.agv = MyAgv(port=port)
         
@@ -36,22 +32,16 @@ class AGV:
         self._run_thread = Thread(target=self._run_thread,daemon=True)
         self._should_run = True
         self._run_thread.start()
-        atexit.register(self._quit)
+        atexit.register(self.quit)
     
     # setters and getters for each state variable
     @property
-    def velocity(self)->Tuple[float,float,float]:
-        """
-        return the current velocity (x, y, rz) values are bounded between -1.0 and 1.0
-        """
+    def velocity(self):
         with self._state_lock:
             return (self._state.x_velocity,self._state.y_velocity,self._state.rz_velocity)
     
     @velocity.setter
-    def velocity(self,vel:Tuple[float,float,float]):
-        """
-        Set velocity (x, y, rz) values are bounded between -1.0 and 1.0
-        """
+    def velocity(self,vel):
         x_velocity,y_velocity,rz_velocity = vel
         for val in (x_velocity,y_velocity,rz_velocity):
             if not -1.0 <= val <= 1.0:
@@ -61,20 +51,27 @@ class AGV:
             self._state.y_velocity = y_velocity
             self._state.rz_velocity = rz_velocity
         print(f"Set velocity to {(x_velocity,y_velocity,rz_velocity)}")
+
+    @property
+    def state(self):
+        with self._state_lock:
+            return self._state.model_copy()
     
-    def _quit(self):
-        """Kills the run thread"""
+    def quit(self):
         with self._state_lock:
             self._should_run = False
     
     def stop(self):
-        """stop the AGV by setting the velocity to 0"""
-        # TODO: Fix this because this is not working quite right (due to the scaling)
+        """stop the AGV"""
         with self._state_lock:
             self._state.x_velocity = 0.0
             self._state.y_velocity = 0.0
             self._state.rz_velocity = 0.0
 
+    def _map_vel(self,vel:float):
+        """scale the speed values from -1 to 1 to 0-255"""
+        return int(127*vel)+128
+    
     def _run_thread(self):
         x_velocity,y_velocity,rz_velocity = 0.0,0.0,0.0
         while True:
@@ -82,11 +79,10 @@ class AGV:
                 new_x_velocity,new_y_velocity,new_rz_velocity = self._state.x_velocity,self._state.y_velocity,self._state.rz_velocity
             if new_x_velocity != x_velocity or new_y_velocity != y_velocity or new_rz_velocity != rz_velocity:
                 # scale values so that they are ints between 0-255
-                # TODO: Scaling is not quite right. Need to fine tune this a bit
-                x_velocity_cmd = 128-int(127*x_velocity)
-                y_velocity_cmd = 128-int(127*y_velocity)
-                rz_velocity_cmd = 128-int(127*rz_velocity)
-
+                x_velocity_cmd = self._map_vel(new_x_velocity)
+                y_velocity_cmd = self._map_vel(new_y_velocity)
+                rz_velocity_cmd = self._map_vel(new_rz_velocity)
+                print(f"sending command {(x_velocity_cmd,y_velocity_cmd,rz_velocity_cmd)}")
                 self.agv._mesg(x_velocity_cmd,y_velocity_cmd,rz_velocity_cmd)
                 
                 # update velocity values
@@ -97,8 +93,15 @@ class AGV:
                 battery_info = self.agv.get_battery_info()
                 if isinstance(battery_info,list) and len(battery_info) == 3:
                     battery_data, v1, v2 = battery_info
+                    battery_data_bits =[bool(int(bit)) for bit in battery_data]
                     with self._state_lock:
                         self._state.battery_status.battery_1.voltage = v1
                         self._state.battery_status.battery_2.voltage = v2
                         self._state.battery_status.time_checked = monotonic()
+
+                        self._state.battery_status.battery_1.charging = battery_data_bits[2]
+                        self._state.battery_status.battery_2.charging = battery_data_bits[4]
+                        self._state.battery_status.battery_1.inserted = battery_data_bits[1]
+                        self._state.battery_status.battery_2.inserted = battery_data_bits[0]
             sleep(0.1)
+    
